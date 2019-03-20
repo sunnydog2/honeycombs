07838fd3a3a48cf7c0e6624a791e1c33ebda88e7
sunnydog2 <sunnydog2@users.noreply.github.com>
2019-03-20 19:07:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,4 @@ function greet(message) {
   alert(message)
 }
 var thing = "surprise!"
-function FizzBuzz() {
-  for(var x=1; x<=15; x++){
-    console.log(x)
-  
-  if(x % 3 === 0){
-    console.log ('Fizz')
-    
-  } else{
-    console.log (x)
-  }
-}
-  
+
